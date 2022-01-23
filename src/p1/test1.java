b12161e42f2399315afc74df2afd4ff9a9a6802d
Omkar Raut <rautom11@gmail.com>
2022-01-23 09:29:53 +0530

    b2

--- src/p1/test1.java
+++ src/p1/test1.java
@@ -1,9 +1,7 @@
 package p1;
 
 public class test1 {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 	System.out.println("In P1 Test1//////Addding");
 	System.out.println("Line 7 P1 Test1 B2 Branch");
 	}
